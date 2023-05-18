28545328c289357a4005344ae5a5f2d4fd8d7284
Angel Acuna <AngelAcuna2000@gmail.com>
2023-05-18 12:40:00 -0400

    created overloads of the add() method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(1.01, 1.02, true);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(double num1, double num2, bool sum)
+        {
+            if (sum == true && num1 + num2 != 1)
+            {
+                return $"{num1 + num2} dollars";
+            }
+
+            else if (sum == true && num1 + num2 == 1)
+            {
+                return $"{num1 + num2} dollar";
+            }
+
+            else
+            {
+                return "0 dollars";
+            }
         }
     }
 }
